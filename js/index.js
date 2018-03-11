58c34b2407c1d20db9f62a26d6ecbfacb297d312
pinkg023 <pinkg023@gmail.com>
2018-03-11 14:42:17 +0800

    set UI and character class

---
+++ js/index.js
@@ -0,0 +1,70 @@
+class BaseCharacter {
+  constructor(name, hp, ap) {
+    this.name = name;
+    this.hp = hp;
+    this.maxHp = hp;
+    this.ap = ap;
+    this.alive = true;
+  }
+  attack(character, damage) {
+    if (this.alive == false) {
+      return;
+    }
+    character.getHurt(damage);
+  }  
+  getHurt(damage) { 
+    this.hp -= damage;
+    if (this.hp <= 0) { 
+      this.die();
+    }
+  }
+  die() {
+    this.alive = false;
+  }
+  updateHtml(hpElement, hurtElement) {
+    hpElement.textContent = this.hp;
+    hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
+  }
+}
+
+class Hero extends BaseCharacter {
+  constructor(name, hp, ap) {
+    super(name, hp, ap);
+    this.element = document.getElementById("hero-image-block");
+    this.hpElement = document.getElementById("hero-hp");
+    this.maxHpElement = document.getElementById("hero-max-hp");
+    this.hurtElement = document.getElementById("hero-hp-hurt");
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+    console.log("召喚英雄！" + this.name + "！");
+  }
+  attack(character) {
+    var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+    super.attack(character, Math.floor(damage));
+  }
+  getHurt(damage) {
+    super.getHurt(damage);                              //沒有修改數值為什麼不用this就好
+    this.updateHtml(this.hpElement, this.hurtElement);  //為什麼不用super繼承updateHtml
+  }
+}
+
+class Monster extends BaseCharacter {
+  constructor(name, hp, ap) {
+    super(name, hp, ap);
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+    console.log("遇到怪獸了！ " + this.name + "！");
+  }
+  attack(character) {
+    var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+    super.attack(character, Math.floor(damage));
+  }
+  getHurt(damage) {
+    super.getHurt(damage);
+    this.updateHtml(this.hpElement, this.hurtElement);
+  }
+}
